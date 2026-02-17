d60264cc2aed8c0bd40a4e07e19f7ed071bdcd59
natsuki-tiida <ito@tiida-works.com>
2026-02-17 10:01:52 +0900

    5章課題を再アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,6 @@
-let heightCm =170
+let heightCm = 170
 let weightKg = 68
 let heightM = heightCm / 100
 
-console.log(weightKg / (heightM * heightM) )
+let bmi = weightKg / (heightM * heightM)
+console.log(bmi)
